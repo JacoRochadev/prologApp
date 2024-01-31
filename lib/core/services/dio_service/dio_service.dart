16160cabc6074963ac431c84703a7bf61e3e0343
JacoRochaDev <jacofernandes@outlook.com>
2024-01-31 16:20:50 -0300

    .

--- lib/core/services/dio_service/dio_service.dart
+++ lib/core/services/dio_service/dio_service.dart
@@ -2,9 +2,10 @@ import 'package:dio/dio.dart';
 import 'package:prolog_app/parameters.dart';
 
 class DioService {
-  static const duration = Duration(milliseconds: 50000);
+  static const duration = Duration(milliseconds: 60000);
   final dio = Dio(
     BaseOptions(
+      connectTimeout: duration,
       baseUrl: baseUrl,
       headers: {
         'Content-Type': 'application/json',
