f04d43ec7e69261d14c3f1a97c7814e68577ada8
JacoRochaDev <jacofernandes@outlook.com>
2024-01-31 16:20:02 -0300

    ok

--- lib/core/services/dio_service/dio_service.dart
+++ lib/core/services/dio_service/dio_service.dart
@@ -1,14 +1,14 @@
 import 'package:dio/dio.dart';
+import 'package:prolog_app/parameters.dart';
 
 class DioService {
   static const duration = Duration(milliseconds: 50000);
   final dio = Dio(
     BaseOptions(
-      baseUrl: "https://prologapp.com/prolog/",
+      baseUrl: baseUrl,
       headers: {
         'Content-Type': 'application/json',
-        'x-prolog-api-token':
-            "ePTC2XrkY34WKUCXq48fTxrLCzdV3M35MNpCfBYavabwwY9BJXg",
+        'x-prolog-api-token': key,
       },
     ),
   );
